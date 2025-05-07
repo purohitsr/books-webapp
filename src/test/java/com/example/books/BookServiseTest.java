29e707c61f388bb02d497acabb321edd42dccc0a
sauravautomation <sauravqa@gmail.com>
2025-05-07 19:28:13 +0530

    updated the test

---
+++ src/test/java/com/example/books/BookServiseTest.java
@@ -0,0 +1,24 @@
+package com.example.books;
+
+
+import java.security.Provider.Service;
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.example.books.Book;
+import com.example.books.BookService;
+
+public class BookServiseTest {
+	
+	@Test
+	public void testGetBookReturnsNonEmptyLists() {
+		BookService service = new BookService();
+		List<Book> books = service.getBooks();
+		
+		Assert.assertFalse(books.isEmpty());
+	    Assert.assertEquals("The Great Gats", books.get(0).getTitle());
+	}
+
+}
